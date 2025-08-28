8f3982a35ec980916f8553e84c95841114fdb446
8388688 <98734038+8388688@users.noreply.github.com>
2025-08-28 19:18:38 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 """
+1.2 更新：
+- 支持 exec 和 download 在特定时间段运行和下载
+- 修复时间戳缺省的问题
+- 日志文件现在以 “*.log” 结尾
+- 优化 disable 选项
+- download 下的小项可以自定义 headers
 1.1-release 紧急补丁：
 - 修正 1.0 以及更低版本无法正确判定版本号的 bug
 1.1 更新【破坏性更新】：
@@ -26,6 +32,7 @@ import colorlog
 import ctypes
 import json
 import logging
+import re
 import requests
 import os
 import sys
@@ -36,8 +43,7 @@ from urllib3.exceptions import ProtocolError
 from requests.exceptions import SSLError, MissingSchema, ConnectionError, InvalidURL, InvalidSchema, RequestException
 # from typing import AnyStr
 
-__version__ = "v1.1"
-CAN_RETRY_CODE = tuple(range(1, 100)) + ()
+__version__ = "v1.2"
 
 
 def is_exec():
@@ -62,6 +68,26 @@ def is_admin() -> bool:
         return False
 
 
+def can_retry(code: int):
+    """
+    code 取值范围：[0..255] 的整数
+    0 表示正常，不会触发重试机制
+    [1..127] 表示会触发重试机制的异常
+    [128..255] 表示【不会】触发重试机制的异常
+    """
+    # assert 0 <= code <= 255
+    if 0 <= code <= 255:
+        if code == 0:
+            return False
+        if code & 128 == 0:
+            return True
+        else:
+            return False
+    else:
+        logger.error("错误，返回代码必须为 0..255 之间的整数")
+        raise ValueError("错误，返回代码必须为 0..255 之间的整数")
+
+
 def decode_version(version_str: str):
     RATE = 1000
     # 高版本号相对低一级的版本号的进制
@@ -78,9 +104,19 @@ def decode_version(version_str: str):
     return ver_code
 
 
-def decode_config_time_version(version_str: str):
-    ver_time = time.mktime(time.strptime(version_str, "%Y.%m.%d.%H.%M.%S"))
+def decode_config_time_version(version_str: str | int):
+    if isinstance(version_str, str):
+        ver_time = time.mktime(time.strptime(version_str, "%Y.%m.%d.%H.%M.%S"))
+    else:
+        assert isinstance(version_str, int)
+        ver_time = version_str
     return ver_time
+
+
+def decode_datetime(date_str: str):
+    return time.mktime(time.strptime(
+        date_str, "%Y/%m/%d"
+    ))
 
 
 def check_attributes(attr):
@@ -90,33 +126,62 @@ def check_attributes(attr):
     return result
 
 
+def check_time(time_str):
+    return re.search(
+        r"(\d+\/\d+\/\d+)?\s*\.\.\s*(\d+\/\d+\/\d+)?",
+        time_str, re.I
+    )
+
+
+def check_time_can_do(config):
+    datetime = check_time(config.get("datetime", globalsettings.get(
+        "datetime", "..")))
+    return datetime.group(0), (not datetime.group(1) or decode_datetime(datetime.group(1)) <= time.time()) and \
+        (not datetime.group(2) or time.time()
+         <= decode_datetime(datetime.group(2)))
+
+
 def run(config: dict):
-    parameters_orig: list[str] = config.get("parameters", fr_json.get("parameters", []))
-    parameters: str = " ".join((str(i) for i in parameters_orig))
+    time_ch = check_time_can_do(config)
     exec_fp: PathLike = config.get("exec")
-    uac_admin: bool = config.get("uac_admin", fr_json.get("uac_admin", False))
-    workdir: PathLike = config.get("workdir", fr_json.get("work_dir", os.getcwd()))
-    fake: bool = config.get("disable", False)
+    parameters_orig: list[str] = config.get(
+        "parameters", globalsettings.get("parameters", []))
+    parameters: str = " ".join((str(i) for i in parameters_orig))
+    uac_admin: bool = config.get(
+        "uac_admin", globalsettings.get("uac_admin", False))
+    workdir: PathLike = config.get(
+        "workdir", globalsettings.get("work_dir", os.getcwd()))
+    fake: bool = config.get("disable", globalsettings.get("disable", False))
     # if is_admin():
     #     logger.info(f"正在使用管理员权限运行 - 非常棒！")
     # else:
     #     logger.info(f"准备以管理员身份重启. . . . . .")
     if not fake:
-        logger.info(f"启动: {exec_fp=}, {parameters_orig=}, {uac_admin=}, {workdir=}")
-        ctypes.windll.shell32.ShellExecuteW(None, "runas" if uac_admin else None, exec_fp, parameters, workdir, 1)
+        if exec_fp is None:
+            logger.error(f"键值对 exec 为必填")
+            return 128
+        if time_ch[1]:
+            logger.info(
+                f"启动: {exec_fp=}, {parameters_orig=}, {uac_admin=}, {workdir=}")
+            ctypes.windll.shell32.ShellExecuteW(
+                None, "runas" if uac_admin else "open", exec_fp, parameters, workdir, 1)
+        else:
+            logger.info(f"启动时间不在 {time_ch[0]} 范围内")
     else:
         logger.info(f"假装启动: {exec_fp=}")
     # sys.exit(0)
 
 
-def download_api(url, file_path):
+def download_api(url, file_path, headers):
     """a simple download script
 
     download file on Internet in silence
     code:
-    [1..99] 是网络问题（会触发重试机制）
-    [100..199] 是用户问题（【不会】触发重试机制）
-    [200..299] 是 OS 问题（【不会】触发重试机制）
+    [1..127] 是网络问题
+    [128..191] 是用户问题
+    [192..255] 是 OS 问题
+    触发重试机制参考 can_retry()
+
     0 = normal(正常)
     1 = unknown error
     2 = Protocol Error
@@ -130,11 +195,11 @@ def download_api(url, file_path):
     10 = 无法辨识的协议
     11 = 协议格式不正确
 
-    100 = 用户禁用了更新
-    101 = Read configure file ERROR: symbiosis-update 键值对为空
-    102 = 强制更新的版本号标志错误
+    128 = 用户禁用了更新
+    129 = Read configure file ERROR: symbiosis-update 键值对为空
+    130 = 强制更新的版本号标志错误
 
-    201 = 保存的目标文件所在的目录不存在
+    192 = 保存的目标文件所在的目录不存在
 
     :param url: the link to download on Internet
     :param file_path: current directory if this param is None else use `file_path`
@@ -144,26 +209,22 @@ def download_api(url, file_path):
     """
     if not os.path.isdir(os.path.dirname(file_path)):
         logger.error(f"无法保存至 {file_path}")
-        return 201
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, '
-                      'like Gecko) Chrome/107.0.0.0 Safari/537.36',
-        # 'Connection': 'close'  # 不使用持久连接
-    }
+        return 192
+    headers = headers
     try:
         r = requests.get(url, stream=True, verify=True, headers=headers)
     except SSLError:
         logger.error(f'SSLError! {url} is not secure.')
-        return 7 # SSL 证书无效或已过期
+        return 7  # SSL 证书无效或已过期
     except MissingSchema:
         logger.error(f"Invalid URL {url}: No scheme supplied")
-        return 11 # 协议格式不正确
+        return 11  # 协议格式不正确
     except InvalidURL:
         logger.error(f"Invalid URL: Failed to parse {url}")
-        return 8 # URL 格式不正确
+        return 8  # URL 格式不正确
     except InvalidSchema:
         logger.error(f"No connection adapters were found for {url}")
-        return 10 # 无法辨识的协议
+        return 10  # 无法辨识的协议
     except ProtocolError as e:
         logger.error(f"ProtocolError")
         for k, v in check_attributes(e.args[0]).items():
@@ -177,9 +238,10 @@ def download_api(url, file_path):
         logger.error(f"Failed to connect {url}: {e.args}")
         # for k, v in check_attributes(e.args[0]).items():
         #     logger.error(f"{k}: {v}")
-        return 9 # 无法连接
+        return 9  # 无法连接
     except RequestException as e:
-        logger.error(f"Request Error {url} - [Error {e.args.count}, {e.args.index}]")
+        logger.error(
+            f"Request Error {url} - [Error {e.args.count}, {e.args.index}]")
         return 1
     except Exception as e:
         logger.error(f"Unexpected Error: {e.args}")
@@ -202,7 +264,8 @@ def download_api(url, file_path):
         for i in r.iter_content(chunk_size=16384):
             f.write(i)
     el = time.time() - st
-    logger.info(f"Download complete, time used: {el:.2f}s, average speed: {f'{filesize / el:.2f}B/s' if filesize != -1 and el != 0 else "?"}.")
+    logger.info(
+        f"Download complete, time used: {el:.2f}s, average speed: {f'{filesize / el:.2f}B/s' if filesize != -1 and el != 0 else "?"}.")
     return 0
 
 
@@ -210,55 +273,69 @@ def download(config):
     "返回值为 0 表示正常下载，非 0 值表示最后一次下载异常退出的错误码（对照表参考 download_api）"
     url = config["url"]
     filepath = config["filepath"]
+    retry = config.get("retry", globalsettings.get("retry", 1))
+    headers = config.get("headers", globalsettings.get("headers", {}))
     if os.path.exists(config["filepath"]) and not config.get("replace-in-force", True):
         tmp = os.path.splitext(filepath)
-        filepath = tmp[0] + time.strftime(".%Y-%m-%d_%H.%M.%S", time.localtime(time.time())) + tmp[-1]  # 前面的 “.” 不能省略
+        filepath = tmp[0] + time.strftime(
+            ".%Y-%m-%d_%H.%M.%S",  # 前面的 “.” 不能省略
+            time.localtime(time.time())) + tmp[-1]
     cnt = 0
     status = 1
-    while not (0 <= config["retry"] - cnt < 1):
-        status = download_api(url, filepath)
-        if status != 0 and status in CAN_RETRY_CODE:
-            cnt += 1
-            logger.info(f"Download failed, still trying... ({cnt}/{config['retry'] if config['retry'] >= 0 else 'Infinity'})")
+    time_ch = check_time_can_do(config)
+    if time_ch[1]:
+        while not (0 <= retry - cnt < 1):
+            status = download_api(url, filepath, headers)
+            if can_retry(status):
+                cnt += 1
+                logger.info(
+                    f"Download failed, still trying... ({cnt}/{retry if retry >= 0 else 'Infinity'})")
+            else:
+                break
         else:
-            break
+            logger.error(f"Discard: {k}")
     else:
-        logger.error(f"Discard: {k}")
+        logger.info(f"启动时间不在 {time_ch[0]} 范围内")
     return status
 
 
 def get_update():
     upgrade_config = fr_json.get("upgrade", dict())
-    local_make_time = fr_json.get("make-time", -1)
-    retry = upgrade_config.get("retry", 1)
+    local_make_time = fr_json.get("make-time", 0)
+    retry = upgrade_config.get("retry", globalsettings.get("retry", 1))
     upgrade_json_fp = get_exec() + ".upgrade"
     upgrade_execute_fp = get_exec() + ".tmp"
     upgrade_old_execute_fp = get_exec() + ".old"
     downgrade_sign = upgrade_config.get("downgrade_install", None)
     upgrade_content = []
     logger.info("Downloading version of configure file. . .")
-    exit_code = download({"url": upgrade_config["json-url"], "filepath": upgrade_json_fp, "retry": retry, "replace-in=force": True})
+    exit_code = download(
+        {"url": upgrade_config["json-url"], "filepath": upgrade_json_fp, "retry": retry, "replace-in=force": True})
     if retry == 0:
         logger.info("更新已禁用")
-        return 100
+        return 128
     if exit_code != 0:
-        logger.error(f"Cannot download version of configure file (Error {exit_code})")
+        logger.error(
+            f"Cannot download version of configure file (Error {exit_code})")
         return exit_code
     logger.info("Resolving configure file. . .")
     with open(upgrade_json_fp, "r", encoding="utf-8") as f:
         head_json = json.loads(f.read())
-    if os.path.exists(upgrade_json_fp): os.unlink(upgrade_json_fp)
+    if os.path.exists(upgrade_json_fp):
+        os.unlink(upgrade_json_fp)
     if upgrade_config.get("enable-config-update", False):
         for k, v in head_json.get("config-file-update", dict()).items():
-            if local_make_time == -1:
-                logger.warning(f"make-time 键值对未设置，Symbiosis 将默认为您填入缺省参数 1970.1.1.0.0.0")
-                local_make_time = "1970.1.1.0.0.0"
+            if local_make_time == 0:
+                logger.warning(
+                    f"make-time 键值对未设置，Symbiosis 将默认为您填入缺省参数 {local_make_time}")
             if decode_config_time_version(k) > decode_config_time_version(local_make_time):
                 upgrade_content.append([k, v])
-        upgrade_content.sort(key=lambda x: decode_config_time_version(x[0]), reverse=True)
+        upgrade_content.sort(
+            key=lambda x: decode_config_time_version(x[0]), reverse=True)
         if upgrade_content:
             if len(upgrade_content) > 1:
-                logger.info(f"检查到多个配置文件的累积更新，{", ".join([i[0] for i in upgrade_content])}")
+                logger.info(
+                    f"检查到多个配置文件的累积更新，{", ".join([i[0] for i in upgrade_content])}")
                 logger.info(f"将自动为您更新到最新的一个版本 {upgrade_content[0][0]}")
             else:
                 pass
@@ -274,14 +351,16 @@ def get_update():
     #########################################################
     if not head_json.get("symbiosis-update", []):
         logger.error("Read configure file ERROR: symbiosis-update 键值对为空")
-        return 101
+        return 129
     if downgrade_sign is not None:
         logger.info(f"发现无视版本的强制更新标志，准备更新至 {downgrade_sign}")
         if downgrade_sign in head_json["symbiosis-update"].keys():
-            download({"url": head_json["symbiosis-update"][downgrade_sign], "filepath": upgrade_execute_fp, "retry": retry, "replace-in=force": True})
+            download({"url": head_json["symbiosis-update"][downgrade_sign],
+                     "filepath": upgrade_execute_fp, "retry": retry, "replace-in=force": True})
         else:
-            logger.error(f"强制更新标志应该是 {', '.join(head_json["symbiosis-update"].keys())} 之一，而不是 {downgrade_sign}")
-            exit_code = 102
+            logger.error(
+                f"强制更新标志应该是 {', '.join(head_json["symbiosis-update"].keys())} 之一，而不是 {downgrade_sign}")
+            exit_code = 130
     else:
         for k, v in head_json["symbiosis-update"].items():
             if decode_version(k) > decode_version(__version__) and k not in upgrade_config.get("specific_version_exclude", []):
@@ -289,12 +368,15 @@ def get_update():
         upgrade_content.sort(key=lambda x: decode_version(x[0]), reverse=True)
         if upgrade_content:
             if len(upgrade_content) > 1:
-                logger.info(f"检查到多个版本的累积更新: {', '.join([i[0] for i in upgrade_content])}")
+                logger.info(
+                    f"检查到多个版本的累积更新: {', '.join([i[0] for i in upgrade_content])}")
                 logger.info(f"将自动为您更新到最新版本 {upgrade_content[0][0]}")
             else:
                 pass
-            download({"url": upgrade_content[0][1], "filepath": upgrade_execute_fp, "retry": retry, "replace-in=force": True})
-            if os.path.exists(upgrade_old_execute_fp): os.unlink(upgrade_old_execute_fp)
+            download(
+                {"url": upgrade_content[0][1], "filepath": upgrade_execute_fp, "retry": retry, "replace-in=force": True})
+            if os.path.exists(upgrade_old_execute_fp):
+                os.unlink(upgrade_old_execute_fp)
             os.rename(get_exec(), upgrade_old_execute_fp)
             os.rename(upgrade_execute_fp, get_exec())
         else:
@@ -317,17 +399,18 @@ def get_update():
     """
 
     # else:
-        # json_0 = req.json()
-        # for i in json_0["content"]:
-            # pass
+    # json_0 = req.json()
+    # for i in json_0["content"]:
+    # pass
 
     return exit_code
 
 
-if not os.path.exists(get_resource("__SymbiosisLogs__")): os.mkdir(get_resource("__SymbiosisLogs__"))
+if not os.path.exists(get_resource("__SymbiosisLogs__")):
+    os.mkdir(get_resource("__SymbiosisLogs__"))
 os.chdir(get_resource())
 console_formatter = colorlog.ColoredFormatter(
-    "%(log_color)s[%(asctime)s.%(msecs)03d] %(filename)s -> %(name)s %(funcName)s line:%(lineno)d [%(levelname)s] : %(message)s",
+    "%(log_color)s[%(asctime)s.%(msecs)03d] %(filename)s -> %(name)s %(funcName)s line:%(lineno)d [%(levelname)s]: %(message)s",
     datefmt="%Y-%m-%dT%H.%M.%SZ",
     log_colors={
         "DEBUG": "white",
@@ -344,9 +427,11 @@ file_formatter = logging.Formatter(
 )
 console = colorlog.StreamHandler()
 console.setFormatter(console_formatter)
-file_log = logging.FileHandler(get_resource(f"__SymbiosisLogs__\\{time.time() // 86400}"), encoding="utf-8")
+file_log = logging.FileHandler(get_resource(
+    f"__SymbiosisLogs__\\{int(time.time() // 86400)}.log"), encoding="utf-8")
 file_log.setFormatter(file_formatter)
-time_rotate_file = logging.handlers.TimedRotatingFileHandler(filename=get_resource(f"__SymbiosisLogs__\\time_rotate"), encoding="utf-8", when="D", interval=1)
+time_rotate_file = logging.handlers.TimedRotatingFileHandler(filename=get_resource(
+    f"__SymbiosisLogs__\\time_rotate"), encoding="utf-8", when="D", interval=1)
 time_rotate_file.setFormatter(file_formatter)
 time_rotate_file.setLevel(logging.INFO)
 
@@ -369,6 +454,8 @@ else:
 fp = os.path.join(get_resource(args.cfgfile))
 with open(fp, "r", encoding="utf-8") as f:
     fr_json: dict = json.loads(f.read())
+globalsettings = fr_json.get("globalsettings", {})
+
 
 """
 {
@@ -379,6 +466,7 @@ with open(fp, "r", encoding="utf-8") as f:
             "uac_admin": bool,
             "workdir": PathLike,
             "disable": false
+            "datetime": "2000/1/1.."
         },
         "2": {
             ...
@@ -390,7 +478,11 @@ with open(fp, "r", encoding="utf-8") as f:
             "filepath": ...,
             "replace-in-force": bool, // true = 替换, false = rename
             "level": [0..4000], // 优先级，决定下载顺序
-            "retry": int, 
+            "headers": {
+                "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36,
+                "Connection": "close"
+            },
+            "retry": int,
             /* 0 = 禁用此项下载
             * 1 = 不重试
             * n（n 为大于 1 的整数）最多 n - 1 次重试
@@ -398,7 +490,7 @@ with open(fp, "r", encoding="utf-8") as f:
             */
             "keep": bool, // 下载完成后是否在配置文件中删除此下载项。
             "expire": bool, // 此项不应由用户设定，表示是否成功下载过该任务。
-        }, 
+        },
         ...
     },
     "upgrade": {
@@ -411,7 +503,14 @@ with open(fp, "r", encoding="utf-8") as f:
         "specific_version_exclude": [], // 空序列表示接受所有更新，序列中的元素表示排除特定版本更新。
         "downgrade_install": "v0.1" || null, // 强制降级安装, null 表示不启用降级安装，此选项无视 specific_version_exclude 的排除设置。
     }
-    "run": {// 仍在可行性分析中}
+    "run": {// 仍在可行性分析中},
+    "globalsettings": {
+        "retry": 1,
+        "uac_admin": false,
+        "keep": true,
+        "parameters": []
+        "disable": false, // 全局禁用，此方案不影响那些显式设置 disable 的元素
+    },
 }
 """
 
